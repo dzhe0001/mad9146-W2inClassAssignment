7edc66a2da65beba172a063e822657e5cf532979
Dmytro Dzhemchuk <dzhemchuk@gmail.com>
2025-05-15 20:11:01 -0400

    Delete Point

--- W2inClassAssignment/Program.cs
+++ W2inClassAssignment/Program.cs
@@ -4,9 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        var p1 = new Point(1, 5);
-        var p2 = new Point(4, 50);
-        Console.WriteLine(p1.getDistance(p2));
-        Console.WriteLine(p1.ToString());
+
     }
 }
