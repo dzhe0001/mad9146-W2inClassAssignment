5cfe9d74a9390d5fbcc17460bfd03eb298446e2c
Dmytro Dzhemchuk <dzhemchuk@gmail.com>
2025-05-19 16:28:48 -0400

    Ex 4 done

---
+++ W2inClassAssignment/Student.cs
@@ -0,0 +1,21 @@
+class Student
+{
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    private int grade;
+    public int Grade
+    {
+        get { return grade; }
+        set
+        {
+            if (value >= 0 && value <= 100)
+            {
+                grade = value;
+            }
+        }
+    }
+}
